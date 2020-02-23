dc275673895fd32993f1b7ce24726769ef434854
Andreas Rottmann <a.rottmann@gmx.at>
2020-02-23 11:04:34 +0100

    Add version check, improve cache handling

--- deb-audit.py
+++ deb-audit.py
@@ -1,9 +1,15 @@
 import dataclasses
 from dataclasses import dataclass
+import os
 from os import path
 import json
+import argparse
 
 import psycopg2
+# TODO: implement the comparison in pure python, it's not worth pulling in a depedency on native
+# code just for that function.
+import apt
+version_compare = apt.apt_pkg.version_compare
 
 @dataclass
 class Issue:
@@ -16,40 +22,56 @@ class Issue:
     fixed_version: str
     status: str
 
+    def is_present_in(self, version):
+        return not self.fixed_version or version_compare(version, self.fixed_version) < 0
+
 class Cache:
-    def __init__(self, *, release, architecture):
+    def __init__(self, *, directory, release, architecture):
+        self._directory = directory
         self._arch = architecture
         self._release = release
         self._source_map = {}
         self._issue_map = {}
 
-    def load(self, directory):
+    def is_present(self):
+        """Check if the cache files are present on disk."""
+        return path.exists(self._source_map_cache()) and path.exists(self._issue_cache())
+
+    def load(self):
         # It's somewhat unfortunate that we are limited to line-delimited JSON here, but it seems
         # Python's json.load always tries to parse the complete input.
         issue_map = {}
-        with open(path.join(directory, f'{self._release}-issues.json')) as f:
+        with open(self._issue_cache()) as f:
             for line in f:
                 json_dict = json.loads(line)
                 issue = Issue(**json_dict)
                 issue_map.setdefault(issue.source, []).append(issue)
         source_map = {}
-        with open(path.join(directory, f'{self._release}-{self._arch}.source-map.json')) as f:
+        with open(self._source_map_cache()) as f:
             json_map = json.load(f)
         for package, versions in json_map.items():
             source_map[package] = [(version, source) for version, source in versions]
         self._issue_map = issue_map
         self._source_map = source_map
 
-    def dump(self, *, directory):
-        pass
+    def dump(self):
+        # TODO: use atomic renames
+        with open(self._source_map_cache(), mode='w') as f:
+            json.dump(self._source_map, f)
+        with open(self._issue_cache(), mode='w') as f:
+            for issues in self._issue_map.values():
+                for issue in issues:
+                    json.dump(dataclasses.asdict(issue), f)
+                    f.write('\n')
 
-    def refresh(self, conn):
-        source_map = fetch_source_map(conn, release=self)
+    def load_udd(self, conn):
+        """Load the cache contents from UDD."""
+        source_map = fetch_source_map(conn, release=self._release, architecture=self._arch)
         issue_map = {}
         for issue in fetch_issues(conn, release=self._release):
             issue_map.setdefault(issue.source, []).append(issue)
         self._source_map = source_map
-        self._issues_map = issue_map
+        self._issue_map = issue_map
 
     def issues(self, *, package):
         versions = self._source_map[package]
@@ -57,6 +79,12 @@ class Cache:
         for source in sources:
             for issue in self._issue_map.get(source, []):
                 yield issue
+
+    def _issue_cache(self):
+        return path.join(self._directory, f'{self._release}-issues.json')
+
+    def _source_map_cache(self):
+        return path.join(self._directory, f'{self._release}-{self._arch}.source-map.json')
 
 def fetch_source_map(conn, *, release, architecture):
     cursor = conn.cursor()
@@ -96,17 +124,6 @@ def fetch_issues(conn, *, release):
                     bug=bug,
                     status=status)
 
-def refresh_cache(conn):
-    source_map = fetch_source_map(conn,
-                                  release='buster',
-                                  architecture='amd64')
-    with open('buster-amd64.source-map.json', mode='w') as f:
-        json.dump(source_map, f)
-    with open('buster-issues.json', mode='w') as f:
-        for issue in fetch_issues(conn, release='buster'):
-            json.dump(dataclasses.asdict(issue), f)
-            f.write('\n')
-
 def udd_connect():
     conn = psycopg2.connect(host='udd-mirror.debian.net',
                             user='udd-mirror',
@@ -116,7 +133,15 @@ def udd_connect():
     return conn
 
 if __name__ == '__main__':
-    cache = Cache(release='buster', architecture='amd64')
-    cache.load('.')
-    for issue in cache.issues(package='zlib1g'):
-        print(issue)
+    cache_dir = path.expanduser('~/.cache/deb-audit')
+    cache = Cache(directory=cache_dir, release='buster', architecture='amd64')
+    if cache.is_present():
+        cache.load()
+    else:
+        os.makedirs(cache_dir)
+        with udd_connect() as conn:
+            cache.load_udd(conn)
+        cache.dump()
+    for issue in cache.issues(package='libxml2'):
+        if issue.is_present_in('2.9.4+dfsg1-8'):
+            print(issue)
